839cbc3e1c83861ec16fe111d29590dd535772bc
leizhang-cs <leizhang.cs@outlook.com>
2020-05-28 18:57:45 +0000

    import samplesort to SAH-build

--- samplesort/samplesort.cpp
+++ samplesort/samplesort.cpp
@@ -1,90 +1,56 @@
 #include "samplesort.h"
-#include "scan.h"
+//#include "scan.h"
+#include "../SAH-build/hierarchy.h"
 using namespace std;
 
 /*
 * Usage: ./ssort [num_elements]
 */
 
-//atomic_int global_index(0);
-int global_index = 0;
-bool debug_flag = false;
-bool print_output = false;
+// explicit instantiation of Samplesort<T>
+template class Samplesort<Entry>;
 
 
-vector<int> pivots;
-vector<int> vec_subarray_count;
-vector<int> vec_bucket_T;
-//vector<int> array_subarray;
-unordered_map<int,int> array_subarray;
-int subarray_size;
-int bucket_size;
-
-
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        cout << "Usage: ./ssort [num_elements]" << endl;
-        return 0;
-    }
-    int n = atoi(argv[1]);
-    cout << n << endl;
-
-    // initialzation
-    int* arr = new int[n];
-    int* output = new int[n];
-
-    cilk_for(int i=0; i<n; i++) arr[i] = hash32(i) % (n*2);
-    vector<int> vec(arr, arr+n);
-
-
-    subarray_size = sqrt(n);
-    int subarray_num = 2*sqrt(n);
-    //vector<int> _array_subarray(n);
-    //swap(array_subarray, _array_subarray);
-    vector<int> _vec_subarray_count(subarray_num*subarray_size);
-    swap(vec_subarray_count, _vec_subarray_count);
-
-    //samplesort
-    timer t; t.start();
-    samplesort(arr, output, n);
-    t.stop(); cout << "time: " << t.get_total() << endl;
-
-    timer tt; tt.start();
-    sort(vec.begin(), vec.end());
-    tt.stop(); cout<<"qsort: "<< tt.get_total() <<endl;
-    int count = 0;
-    for(int i=0; i<vec.size(); i++){
-        if(vec[i]!=output[i]){
-            count++;
-        }
-    }
-    cout<<"diff:"<<count<<endl;
-
-    return 0;
+template<class T>
+inline uint32_t Samplesort<T>::hash32(uint32_t a) {
+    a = (a+0x7ed55d16) + (a<<12);
+    a = (a^0xc761c23c) ^ (a>>19);
+    a = (a+0x165667b1) + (a<<5);
+    a = (a+0xd3a2646c) ^ (a<<9);
+    a = (a+0xfd7046c5) + (a<<3);
+    a = (a^0xb55a4f09) ^ (a>>16);
+    if (a<0) a = -a;
+    return a;
 }
+
+template<class T>
+inline double Samplesort<T>::LOG(int x){
+    return log(x)/log(2);
+}
+
 
 // N^1/2 sort
 // N^1/2*logn, every logn -> pivot
-// dac_sort write to B
-void samplesort(int* arr, int* output, int n){
+template<class T>
+void Samplesort<T>::sort(vector<T>& arr, vector<T>& output){
     bool _debug_flag = false;
-
-    int bucket_num = sqrt(n);
-    vector<int> _pivots(bucket_num-1);
-    swap(pivots, _pivots);
-    /*
+    int n = arr.size();
     subarray_size = sqrt(n);
-    int subarray_num = 2*sqrt(n);
-    //vector<int> _array_subarray(n);
-    //swap(array_subarray, _array_subarray);
+    bucket_num = sqrt(n);
+    vector<T> _pivots(bucket_num-1);
+    swap(pivots, _pivots);
+    
+    subarray_size = sqrt(n);
+    subarray_num = 2*sqrt(n);
     vector<int> _vec_subarray_count(subarray_num*subarray_size);
     swap(vec_subarray_count, _vec_subarray_count);
-    */
+    //std::cout<<subarray_size<<std::endl;
+    
     // sample
     sampling(arr, n);
     if(_debug_flag || debug_flag){
         cout<<endl<<"arr:"<<endl;
-        for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+        //for(int i=0; i<n; i++) cout<<arr[i]<<" ";
         cout<<endl;
     }
 
@@ -93,7 +59,7 @@ void samplesort(int* arr, int* output, int n){
     dac_sort_subarray(arr, 0, n, subarray_size);
     if(_debug_flag || debug_flag){
         cout<<endl<<"subarray:"<<endl;
-        for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+        //for(int i=0; i<n; i++) cout<<arr[i]<<" ";
         cout<<endl;
     }
 
@@ -102,54 +68,57 @@ void samplesort(int* arr, int* output, int n){
     transpose(bucket_num, bucket_size);
     if(_debug_flag || debug_flag){
         cout<<endl<<"T:"<<endl;
-        for(auto x: vec_bucket_T) cout<<x<<" "; cout<<endl;
+        //for(auto x: vec_bucket_T) cout<<x<<" "; cout<<endl;
     }
 
     // scan
-    //for(int i=1; i<vec_bucket_T.size(); i++) vec_bucket_T[i] += vec_bucket_T[i-1];
-    int* temp = &vec_bucket_T.front();
+    for(size_t i=1; i<vec_bucket_T.size(); i++) vec_bucket_T[i] += vec_bucket_T[i-1];
+    /*int* temp = &vec_bucket_T.front();
     int* e1 = new int[bucket_num*bucket_size];
     int* e2 = new int[bucket_num*bucket_size];
-    scan(temp, temp, e1, e2, vec_bucket_T.size());
+    scan(temp, temp, e1, e2, vec_bucket_T.size());*/
     if(_debug_flag || debug_flag){
         cout<<"scan:"<<endl;
-        for(auto x: vec_bucket_T) cout<<x<<" "; cout<<endl;
+        //for(auto x: vec_bucket_T) cout<<x<<" "; cout<<endl;
     }
 
     // merge bucket
     dac_merge(arr, output, 0, n, subarray_size);
     if(_debug_flag || debug_flag){
         cout<<endl<<"merge:"<<endl;
-        for(int i=0; i<n; i++) cout<<output[i]<<" "; cout<<endl;
+        //for(int i=0; i<n; i++) cout<<output[i]<<" "; cout<<endl;
     }
     // sort bucket
     dac_sort_bucket(output, 0, bucket_num);
     if(print_output){
         if(debug_flag || _debug_flag) cout<<endl<<"output:"<<endl;
-        for(int i=0; i<n; i++) cout<<output[i]<<" "; cout<<endl;
+        //for(int i=0; i<n; i++) cout<<output[i]<<" "; cout<<endl;
     }
 }
 
-void dac_sort_bucket(int* arr, int b_begin, int b_end){
+template<class T>
+void Samplesort<T>::dac_sort_bucket(vector<T>& arr, int b_begin, int b_end){
     int n = b_end - b_begin;
     if(n<=0) return;
     if(n==1){
         int arr_begin = b_begin*bucket_size>0? vec_bucket_T[b_begin*bucket_size-1]: 0,
             arr_end = vec_bucket_T[b_end*bucket_size-1];
         if(debug_flag) cout<<"bucket:"<<b_begin<<", arr:"<<arr_begin<<" "<<arr_end<<endl;
-        quicksort(arr, arr_begin, arr_end);
+        // local_begin, local_end + output_global_begin
+        std::sort(arr.begin()+arr_begin+output_begin, arr.begin()+arr_end+output_begin, comp);
     }
     else{
-        cilk_spawn
+        /*cilk_spawn*/
         dac_sort_bucket(arr, b_begin, b_begin+n/2);
         dac_sort_bucket(arr, b_begin+n/2, b_end);
-        cilk_sync;
+        /*cilk_sync;*/
     }
 }
 
 
 // move elements of a subarray into corresponding buckets
-void dac_merge(int* arr, int* output, int begin, int end, int th){
+template<class T>
+void Samplesort<T>::dac_merge(vector<T>& arr, vector<T>& output, int begin, int end, int th){
     int n = end - begin;
     if(n<=0) return;
     if(n<=th){
@@ -160,22 +129,23 @@ void dac_merge(int* arr, int* output, int begin, int end, int th){
         for(int i=begin, j=0; j<th; j++){
             int o_begin = j*bucket_size+s_i>0? vec_bucket_T[j*bucket_size+s_i-1]: 0,
                 o_end = vec_bucket_T[j*bucket_size+s_i];
-
-            for(int k=o_begin; k<o_end; k++, i++){
+            // output_index = local_index + output_global_begin
+            for(int k=o_begin+output_begin; k<o_end+output_begin; k++, i++){
                 output[k] = arr[i];
             }
         }
         //cout<<begin<<" "<<i<<", "<<s->begin<<" "<<s->end<<endl;
     }
     else{
-        cilk_spawn
+        /*cilk_spawn*/
         dac_merge(arr, output, begin, begin+n/2, th);
         dac_merge(arr, output, begin+n/2, end, th);
-        cilk_sync;
+        /*cilk_sync;*/
     }
 }
 
-void dac_allocate(int begin, int end, int th){
+template<class T>
+void Samplesort<T>::dac_allocate(int begin, int end, int th){
     int n = end - begin;
     if(n<=th){
         array_subarray[begin] = global_index++;
@@ -187,25 +157,21 @@ void dac_allocate(int begin, int end, int th){
 }
 
 // th = bucket_size = pivots.size()+1
-void dac_sort_subarray(int* arr, int begin, int end, int th){
+template<class T>
+void Samplesort<T>::dac_sort_subarray(vector<T>& arr, int begin, int end, int th){
     int n = end - begin;
     if(n<=0) return;
     if(n<=th){
-        //int local_index = global_index++;
-        //while(!atomic_compare_exchange_strong(&global_index, &local_index, local_index+1)){
-        //    local_index = global_index;
-        //}    
-        //array_subarray[begin] = local_index;
-
         int local_index = array_subarray[begin];
-        quicksort(arr, begin, end);
+        std::sort(arr.begin()+begin, arr.begin()+end, comp);
 
         int i=begin, j=0, offset=local_index*subarray_size;
         int count=0;
         //cout<<"offset: "<<offset<<endl;
 
         while(i<end && j<th-1){
-            if(arr[i]<pivots[j]){
+            // TODO
+            if(comp.operator()(arr[i],pivots[j])){
                 count++;
                 i++;
             }
@@ -227,19 +193,20 @@ void dac_sort_subarray(int* arr, int begin, int end, int th){
         }
     }
     else{
-        cilk_spawn
+        /*cilk_spawn*/
         dac_sort_subarray(arr, begin, begin+n/2, th);
         dac_sort_subarray(arr, begin+n/2, end, th);
-        cilk_sync;
+        /*cilk_sync;*/
     }
 }
 
-void transpose(int bucket_num, int bucket_size){
+template<class T>
+void Samplesort<T>::transpose(int bucket_num, int bucket_size){
     // bucket_num = sqrt(n), bucket_size = global_index
     vector<int> _vec_bucket_T(bucket_num*bucket_size);
     swap(vec_bucket_T, _vec_bucket_T);
 
-    cilk_for(int i=0; i<bucket_num; i++){
+    /*cilk_for*/for(int i=0; i<bucket_num; i++){
         int offset = i * bucket_size;
         for(int j=0; j<bucket_size; j++){
             vec_bucket_T[offset+j] = vec_subarray_count[j*subarray_size+i];
@@ -248,55 +215,45 @@ void transpose(int bucket_num, int bucket_size){
 }
 
 
-void sampling(int* arr, int n){
-    // TODO
+template<class T>
+void Samplesort<T>::sampling(const vector<T>& arr, int n){
+    bool _debug_flag = false;
+
     int bucket_size = sqrt(n), logn = LOG(n);
     int bucket_num = (n-1)/bucket_size + 1;
-    int* bucket_index = new int[bucket_num];
-    int* sample_bucket = new int[logn];
+    vector<int> bucket_index(bucket_num);
+    vector<int> sample_bucket(logn);
 
     // choose logn buckets randomly
     for(int i=0; i<bucket_num; i++) bucket_index[i] = i;
+    //
     for(int i=0; i<logn; i++){
-        int j = hash32(i)%bucket_num;
-        sample_bucket[i] = j;
-        swap(bucket_index[i], bucket_index[j]);
+        int j = hash32(i)%(bucket_num-i);
+        sample_bucket[i] = bucket_index[j];
+        swap(bucket_index[logn-1-i], bucket_index[j]);
     }
-
+    if(_debug_flag) cout<<"sampling()"<<endl;
     // N^1/2*logn samples
-    vector<int> arr_sample(bucket_size*logn);
+    vector<T> arr_sample(bucket_size*logn);
     for(int i=0; i<logn; i++){
         int offset1 = bucket_size*i;
         int offset2 = bucket_size*sample_bucket[i];
         for(int j=0; j<bucket_size; j++){
+            // cout<<"ij: "<<offset1+j<<" "<<arr_sample.size()<<endl;
+            // cout<<"ij: "<<sample_bucket[i]<<" "<<offset2+j<<" "<<arr.size()<<endl;
             arr_sample[offset1+j] = arr[offset2+j];
         }
     }
-
-    sort(arr_sample.begin(), arr_sample.end());
+    if(_debug_flag) cout<<"sampling sort"<<endl;
+    std::sort(arr_sample.begin(), arr_sample.end(), comp);
     // N^1/2 - 1 pivots
 
+    if(_debug_flag) cout<<"sampling pivots assign"<<endl;
     for(int i=0; i<bucket_size-1; i++){
-        if(debug_flag && i==0) cout<<"pivots: ";
         pivots[i] = arr_sample[logn*(i+1)];
-        if(debug_flag){
-            cout<<pivots[i]<<" ";
-            if(i==bucket_size-2) cout<<endl;
-        }
-    }
-}
-
-void quicksort(int* nums, int begin, int end){
-    if(begin<end-1){
-        int i = begin, j = end-1, key = nums[begin];
-        while(i<j){
-            while(key<nums[j] && i<j) j--;
-            if(i<j) nums[i++] = nums[j];
-            while(nums[i]<key && i<j) i++;
-            if(i<j) nums[j--] = nums[i];
-        }
-        nums[i] = key;
-        quicksort(nums, begin, i);
-        quicksort(nums, i+1, end);
+        // if(_debug_flag){
+        //     cout<<pivots[i]<<" ";
+        //     if(i==bucket_size-2) cout<<endl;
+        // }
     }
 }
--- samplesort/samplesort.h
+++ samplesort/samplesort.h
@@ -16,42 +16,52 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <atomic>
-#include <cilk/cilk.h>
-#include <cilk/cilk_api.h>
+//#include <cilk/cilk.h>
+//#include <cilk/cilk_api.h>
 #include "get_time.h"
+#include "../SAH-build/common.h"
+//#include "common.h"
 
-uint32_t hash32(uint32_t a);
-void quicksort(int* nums, int begin, int end);
-void samplesort(int* arr, int* output, int n);
-void sampling(int* arr, int n);
-void dac_sort_subarray(int* arr, int begin, int end, int th);
-void transpose(int bucket_num, int bucket_size);
-void dac_merge(int* arr, int* output, int begin, int end, int th);
-void dac_sort_bucket(int* arr, int b_begin, int b_end);
-void dac_allocate(int begin, int end, int th);
-double LOG(int x);
 
-struct subarray{
-    int index;
-    int begin;
-    int end;
-    subarray(){}
-    subarray(int _index, int _begin, int _end)
-        :index(_index), begin(_begin), end(_end)
+template<class T>
+class Samplesort{
+public:
+    Samplesort(compare comp_input)
+    :comp(comp_input),output_begin(0)
     {}
+    // if output starts from vector.begin()+output_begin
+    Samplesort(compare comp_input, int output_begin_input)
+    :comp(comp_input), output_begin(output_begin_input)
+    {}
+
+    void sort(std::vector<T>& arr, std::vector<T>& output);
+    uint32_t hash32(uint32_t a);
+
+private:
+    compare comp;
+    int output_begin;
+    //atomic_int global_index(0);
+    int global_index = 0;
+    int subarray_size;
+    int bucket_size;
+    int bucket_num;
+    int subarray_num;
+    std::vector<T> pivots;
+    std::vector<int> vec_subarray_count;
+    std::vector<int> vec_bucket_T;
+    std::unordered_map<int,int> array_subarray;  
+
+    bool debug_flag = false;
+    bool print_output = false;  
+
+    void sampling(const std::vector<T>& arr, int n);
+    void dac_sort_subarray(std::vector<T>& arr, int begin, int end, int th);
+    void transpose(int bucket_num, int bucket_size);
+    void dac_merge(std::vector<T>& arr, std::vector<T>& output, int begin, int end, int th);
+    void dac_sort_bucket(std::vector<T>& arr, int b_begin, int b_end);
+    void dac_allocate(int begin, int end, int th);
+    //bool compare(const T& a, const T& b, const int dimension);
+    double LOG(int x);
 };
 
-inline uint32_t hash32(uint32_t a) {
-        a = (a+0x7ed55d16) + (a<<12);
-        a = (a^0xc761c23c) ^ (a>>19);
-        a = (a+0x165667b1) + (a<<5);
-        a = (a+0xd3a2646c) ^ (a<<9);
-        a = (a+0xfd7046c5) + (a<<3);
-        a = (a^0xb55a4f09) ^ (a>>16);
-        if (a<0) a = -a;
-        return a;
-}
 
-inline double LOG(int x){
-    return log(x)/log(2);
-}
